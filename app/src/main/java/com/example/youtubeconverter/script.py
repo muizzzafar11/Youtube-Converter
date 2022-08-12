3e1109a6071e94515056cfdbd9360a02a92a7f00
Muizz Zafar <muizzzafar25351964@gmail.com>
2022-08-12 04:58:04 -0400

    updated script to get best audio quality

--- app/src/main/java/com/example/youtubeconverter/script.py
+++ app/src/main/java/com/example/youtubeconverter/script.py
@@ -1,12 +1,13 @@
 from pytube import YouTube
 import os
+# import eyed3
 
 def converter(url: str, destination: str):
     # url input from user
     yt = YouTube(url)
 
     # extract only audio
-    video = yt.streams.filter(only_audio=True).first()
+    video = yt.streams.get_audio_only()
 
     # download the file
     out_file = video.download(output_path=destination)
@@ -15,6 +16,13 @@ def converter(url: str, destination: str):
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
+
+#     mp3File = eyed3.load(new_file)
+#     mp3File.tag.artist = yt.artist
+#     mp3File.tag.album = yt.album
+#     mp3File.tag.album_artist = yt.album_artist
+
+#     mp3File.tag.save()
 
     # result of success
     return yt.title + " has been successfully downloaded."
